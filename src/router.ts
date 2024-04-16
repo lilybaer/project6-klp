56dc79634fd8e1b2c976aab5846ed59c21c8f505
Kate Ottenwess <90808295+kateottenwess@users.noreply.github.com>
2024-04-16 15:25:37 -0400

    forgot to add router so here that is

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,9 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "./components/Home.vue";
+import Clothing from "./components/Clothing.vue";
+import Electronics from "./components/Electronics.vue";
+import Groceries from "./components/Groceries.vue";
+import BestSeller from "./components/BestSeller.vue";
 const routes = [
   {
     path: "/",
@@ -8,6 +12,26 @@ const routes = [
     component: HomeView,
   },
   // you need to add more routes here
+  {
+    path: "/clothing",
+    name: "clothing",
+    component: Clothing,
+  },
+  {
+    path: "/electronics",
+    name: "electronics",
+    component: Electronics,
+  },
+  {
+    path: "/groceries",
+    name: "groceries",
+    component: Groceries,
+  },
+  {
+    path: "/bestseller",
+    name: "bestseller",
+    component: BestSeller,
+  },
 ];
 
 const router = createRouter({
